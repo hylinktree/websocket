e25f4f7d59f349a1170bd9902d27dbc4b80420f7
玄義 <hylinktree@gmail.com>
2021-07-30 16:39:49 +0800

    nocomment

--- examples/mongo/main.go
+++ examples/mongo/main.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -85,10 +87,18 @@ func getconn() {
 	var i = 0
 
 	for ; i < *runs; i++ {
-		ash := Trainer{"Ash", 10, "Pallet Town", randvoltage(), randvoltage(), randvoltage()}
+		// ash := Trainer{"Ash", 10, "Pallet Town", randvoltage(), randvoltage(), randvoltage()}
 		collection := client.Database("test").Collection("trainers")
 
-		_, err := collection.InsertOne(ctx, ash)
+		_, err := collection.InsertOne(ctx, bson.D{
+			{"family", "brian"},
+		})
+
+		// _, err := collection.InsertMany(ctx, []interface{}{
+		// 	bson.D{
+		// 		{"Name", "podcastResult.InsertedID"},
+		// 	},
+		// })
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -111,13 +121,96 @@ func getconn() {
 }
 
 var (
-	host = flag.String("host", "hpcargo", "host")
-	port = flag.String("port", "27017", "port")
-	runs = flag.Int("runs", 1, "total runs")
+	host       = flag.String("host", "hpcargo", "host")
+	port       = flag.String("port", "27017", "port")
+	runs       = flag.Int("runs", 1, "total runs")
+	database   = flag.String("db", "postgres", "the database for run")
+	collection = flag.String("co", "meter101", "the collection for run")
 )
 
+func getconn2() {
+
+	ctx := context.TODO()
+	// As you set the timeout, it is expected the operation should be completed within the time!!
+	// ctx, cancel := context.WithTimeout(context.Background(), timeout*time.Second)
+	// defer cancel()
+	uri := "mongodb://admin:secret@" + *host + ":" + *port
+	clientOptions := options.Client().ApplyURI(uri)
+
+	// Connect to MongoDB
+	fmt.Println("connecting to", uri)
+	client, err := mongo.Connect(ctx, clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		client.Disconnect(ctx)
+		fmt.Println("disconnected!")
+	}()
+	var i = 0
+
+	for ; i < *runs; i++ {
+		// ash := Trainer{"Ash", 10, "Pallet Town", randvoltage(), randvoltage(), randvoltage()}
+		collection := client.Database(*database).Collection(*collection)
+
+		var err error
+
+		_, err = collection.InsertOne(ctx, bson.D{
+			{"voltageR", randvoltage()},
+			{"voltageS", randvoltage()},
+			{"voltageT", randvoltage()},
+			{"timeStamp", time.Now().Unix()}, //makeJavaTimestamp(time.Now())},
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = collection.InsertMany(ctx, []interface{}{
+			bson.D{
+				{"voltageR", randvoltage()},
+				{"voltageS", randvoltage()},
+				{"voltageT", randvoltage()},
+				{"timeStamp", time.Now().Unix()}}, //makeJavaTimestamp(time.Now())},
+			bson.D{
+				{"voltageR", randvoltage()},
+				{"voltageS", randvoltage()},
+				{"voltageT", randvoltage()},
+				{"timeStamp", time.Now().Unix()}},
+		})
+
+		// _, err := collection.InsertMany(ctx, []interface{}{
+		// 	bson.D{
+		// 		{"Name", "podcastResult.InsertedID"},
+		// 	},
+		// })
+		if err != nil {
+			log.Fatal(err)
+		}
+		if i != 0 && (i%1000) == 0 {
+			fmt.Printf("%d records sent\n", i)
+		}
+	}
+	fmt.Printf("total %d records sent\n", i)
+
+	// fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	// Check the connection
+	err = client.Ping(ctx, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+}
+
+func makeJavaTimestamp(t time.Time) int64 {
+	return int64(time.Nanosecond) * t.UnixNano() / int64(time.Millisecond)
+}
 func main() {
 
 	flag.Parse()
-	getconn()
+	getconn2()
 }
